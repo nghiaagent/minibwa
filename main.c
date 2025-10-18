5ad6934e7c77f675557fcda488e7d173a1038d0b
Heng Li <lh3@me.com>
2025-10-18 11:00:03 -0400

    to fix multi-sa; buggy right now

--- main.c
+++ main.c
@@ -65,32 +65,35 @@ int main(int argc, char *argv[])
 	return 0;
 }
 
-typedef enum { MB_BENCH_2A, MB_BENCH_SA } mb_bench_type_t;
+typedef enum { MB_BENCH_2A, MB_BENCH_SA, MB_BENCH_MSA } mb_bench_type_t;
 
 int main_bench(int argc, char *argv[])
 {
 	mb_bench_type_t type = MB_BENCH_2A;
 	uint64_t x = 11, cs = 1;
 	int64_t i, n = 1000000;
-	int c, print_val = 0;
+	int c, print_val = 0, intv = 20;
 	mb_bwt_t *bwt;
 	ketopt_t o = KETOPT_INIT;
 	double t;
 
-	while ((c = ketopt(&o, argc, argv, 1, "pn:b:", 0)) >= 0) {
+	while ((c = ketopt(&o, argc, argv, 1, "pn:b:v:", 0)) >= 0) {
 		if (c == 'n') n = kom_parse_num(o.arg, 0);
 		else if (c == 'p') print_val = 1;
+		else if (c == 'v') intv = atoi(o.arg);
 		else if (c == 'b') {
 			if (strcmp(o.arg, "2a") == 0) type = MB_BENCH_2A;
 			else if (strcmp(o.arg, "sa") == 0) type = MB_BENCH_SA;
+			else if (strcmp(o.arg, "msa") == 0) type = MB_BENCH_MSA;
 			else kom_assert(0, "unknown type");
 		}
 	}
 	if (argc - o.ind < 1) {
 		fprintf(stderr, "Usage: minibwa bench [options] <in.mbw>\n");
 		fprintf(stderr, "Options:\n");
-		fprintf(stderr, "  -b STR         type: 2a or sa [2a]\n");
+		fprintf(stderr, "  -b STR         type: 2a, sa or msa [2a]\n");
 		fprintf(stderr, "  -n NUM         number of data points [10k]\n");
+		fprintf(stderr, "  -v INT         interval size for msa [%d]\n", intv);
 		fprintf(stderr, "  -p             print results for each data point\n");
 		return 1;
 	}
@@ -112,6 +115,24 @@ int main_bench(int argc, char *argv[])
 			s = mb_bwt_sa(bwt, k);
 			cs = (cs >> 32) ^ s;
 			if (print_val) printf("%lld\n", s);
+		}
+	} else if (type == MB_BENCH_MSA) {
+		for (i = 0; i < n; ++i) {
+			uint64_t j, xor = 0, k = kom_splitmix64(&x) % bwt->seq_len;
+			uint64_t l = k + intv < bwt->seq_len? k + intv : bwt->seq_len;
+			#if 0
+			for (j = k; j < l; ++j) {
+				uint64_t s = mb_bwt_sa(bwt, j);
+				xor ^= s;
+			}
+			#else
+			uint64_t sa[intv], n_sa;
+			n_sa = mb_bwt_sa_multi(0, bwt, k, l, intv, sa);
+			for (j = 0; j < n_sa; ++j)
+				xor ^= sa[j];
+			#endif
+			cs = (cs >> 32) ^ xor;
+			if (print_val) printf("%lld\n", xor);
 		}
 	}
 	fprintf(stderr, "checksum = %lx\n", (unsigned long)cs);
