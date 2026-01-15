9b92e93c819e1960f5855be9a599c1c070c9fe65
Heng Li <lh3@me.com>
2026-01-15 14:48:46 -0500

    output s2

--- format.c
+++ format.c
@@ -10,6 +10,7 @@ void mb_fmt_paf_basic(kstring_t *s, const l2b_t *l2b, int64_t qlen, const mb_hit
 		kom_sprintf_lite(s, "\t%d\t%d\t%c\t%s\t%ld\t%ld\t%ld\t%d\t%d\t%d\ttp:A:%c\ts1:i:%d\tcm:i:%d",
 			p->qs, p->qe, p->rev? '-' : '+', l2b->ctg[p->tid].name, (long)l2b->ctg[p->tid].len, (long)p->ts, (long)p->te,
 			p->mlen, p->blen, p->mapq, p->parent == p->id? 'P' : 'S', p->score, p->cnt);
+		if (p->parent == p->id) kom_sprintf_lite(s, "\ts2:i:%d", p->subsc);
 		if (p->p)
 			kom_sprintf_lite(s, "\tAS:i:%d\tms:i:%d", p->p->dp_score, p->p->dp_max);
 		kom_sprintf_lite(s, "\n");
